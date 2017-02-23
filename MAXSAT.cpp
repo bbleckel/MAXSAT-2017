c9989f9d481725fa14d74871c77ad8446a6df971
bbleckel <lbleckel@bowdoin.edu>
2017-02-22 19:54:59 -0500

    hopefully successfully merged eveything

--- MAXSAT.cpp
+++ MAXSAT.cpp
@@ -112,14 +112,15 @@ void MaxSat::initPV() {
 }
 
 void MaxSat::initPopulation() {
-    int randNum;
-    for (int i = 0; i < individuals; i++) {
-        for (int j = 0; j < numVariables; j++) {
-            // give random values to each individual
-            randNum = rand() % 2;
-            population[i][j] = randNum;
-        }
-    }
+	//initialize population for GA with each individual
+	//holding a random value of 1 or 0
+	int randNum;
+	for (int i = 0; i < individuals; i++) {
+		for (int j = 0; j < numVariables; j++) {
+			randNum = rand() % 2;
+			population[i][j] = randNum;
+		}
+	}
 }
 
 int MaxSat::countSatClauses(int* solution) {
@@ -198,276 +199,6 @@ int compare (const void *pa, const void *pb) {
     const int *b = *(const int **)pb;
     
     return a[1] - b[1];
-}
-
-void MaxSat::arrayCopy(int* arr1, int* arr2, int size) {
-    for (int i = 0; i < size; i++) {
-        arr1[i] = arr2[i];
-    }
-}
-
-void MaxSat::selectRanking() {
-    // a 2D array to store the index and fitness of each individual
-    // this array will be sorted by fitness
-    int** rankList = (int**) malloc(sizeof(int) * individuals  * 2);
-    for (int i = 0; i < individuals; i++) {
-        rankList[i] = (int*) malloc(sizeof(int) * 2);
-        
-        rankList[i][0] = i;
-        rankList[i][1] = fitnessList[i];
-    }
-    
-    //quicksort to sort the individuals by fitness (while maintaining index)
-    qsort(rankList, individuals, sizeof rankList[0], compare);
-    
-    double sum = (individuals * (individuals + 1))/2;
-    
-    for (int i = 0; i < individuals; i++) {
-        double probability = 0;
-        double randomProbability = ((double) rand())/(INT_MAX);
-        for (int j = 0; j < individuals; j++) {
-            probability += (j + 1) / sum;
-            
-            if (probability >= randomProbability) {
-                arrayCopy(breedingPool[i], population[rankList[j][0]], numVariables);
-            }
-        }
-    }
-    
-    free(rankList);
-}
-
-void MaxSat::selectTournament() {
-    int randNum;
-    int* individual1 = (int*) malloc(sizeof(int) * numVariables);
-    int* individual2 = (int*) malloc(sizeof(int) * numVariables);
-    for (int i = 0; i < individuals; i++) {
-        randNum = rand() % individuals;
-        arrayCopy(individual1, population[randNum], numVariables);
-        int fitness1 = fitnessList[randNum];
-        
-        randNum = rand() % individuals;
-        arrayCopy(individual2, population[randNum], numVariables);
-        int fitness2 = fitnessList[randNum];
-        if (fitness1 > fitness2) {
-            arrayCopy(breedingPool[i], individual1, numVariables);
-        } else {
-            arrayCopy(breedingPool[i], individual2, numVariables);
-        }
-    }
-    free(individual1);
-    free(individual2);
-}
-
-void MaxSat::selectBoltzmann() {
-    int i = 0;
-    
-    double totalFitness = 0;
-    for (int n = 0; n < individuals; n++) {
-        totalFitness += exp(fitnessList[n]);
-    }
-    
-    for (int i = 0; i < individuals; i++) {
-        double probability = 0;
-        double randomProbability = ((double) rand())/(INT_MAX);
-        for (int j = 0; j < individuals; j++) {
-            probability += exp(fitnessList[j])/totalFitness;
-            
-            if (probability >= randomProbability) {
-                arrayCopy(breedingPool[i], population[j], numVariables);
-            }
-        }
-    }
-}
-
-void MaxSat::onePCross() {
-    for (int i = 0; i < individuals; i += 2) {
-        double randNum = ((double) rand())/(RAND_MAX);
-        if (randNum < pC){
-            int* parent1 = breedingPool[i];
-            int* parent2 = breedingPool[i+1];
-            double crossPointRand = ((double) rand())/(RAND_MAX);
-            int crossPoint = (int) (crossPointRand * numVariables);
-            int* offspring1 = (int*) malloc(sizeof(int) * numVariables);
-            int* offspring2 = (int*) malloc(sizeof(int) * numVariables);
-            
-            for (int j = 0; j < numVariables; j++) {
-                if (j < crossPoint) {
-                    offspring1[j] = parent1[j];
-                    offspring2[j] = parent2[j];
-                } else {
-                    offspring1[j] = parent2[j];
-                    offspring2[j] = parent1[j];
-                }
-            }
-            arrayCopy(population[i], offspring1, numVariables);
-            arrayCopy(population[i+1], offspring2, numVariables);
-            free(offspring1);
-            free(offspring2);
-        } else {
-            arrayCopy(population[i], breedingPool[i], numVariables);
-            arrayCopy(population[i+1], breedingPool[i+1], numVariables);
-        }
-    }
-}
-
-void MaxSat::uniformCross() {
-    for (int i = 0; i < individuals; i++) {
-        double randNum = ((double) rand())/(RAND_MAX);
-        if (randNum < pC) {
-            int* parent1 = breedingPool[i];
-            int* parent2;
-            if (i+1 == individuals) {
-                parent2 = breedingPool[0];
-            } else {
-                parent2 = breedingPool[i+1];
-            }
-            // should only malloc & free once if necessary
-            int* offspring = (int*) malloc(sizeof(int) * numVariables);
-            
-            for (int j = 0; j < numVariables; j++) {
-                double pointProb = ((double) rand())/(RAND_MAX);
-                if (pointProb < 0.5) {
-                    offspring[j] = parent1[j];
-                } else {
-                    offspring[j] = parent2[j];
-                }
-            }
-            arrayCopy(population[i], offspring, numVariables);
-            free(offspring);
-        } else {
-            arrayCopy(population[i], breedingPool[i], numVariables);
-        }
-    }
-}
-
-void MaxSat::mutatePV() {
-    double mutRand;
-    int dirRand;
-    int mutDirection;
-    for(int i = 0; i < PV.size() - 1; i++) {
-        mutRand = ((double) rand())/(RAND_MAX);
-        // mutate or not?
-        if(mutRand < pM) {
-            dirRand = rand() % 100;
-            // choose mutation direction
-            if((double) dirRand / 100 < 0.5) {
-                mutDirection = 1;
-            } else {
-                mutDirection = 0;
-            }
-            // make mutation
-            PV[i] = PV[i] * (1.0 - mutAmnt) + mutDirection * mutAmnt;
-        }
-    }
-}
-
-void MaxSat::mutateOffspring() {
-    int mutRand;
-    for(int i = 0; i < individuals; i++) {
-        for(int j = 0; j < numVariables; j++) {
-            mutRand = rand() % 100;
-            if((double) mutRand / 100 < pM) {
-                // make mutation: reverse the value of the "bit"
-                population[i][j] = !population[i][j];
-            }
-        }
-    }
-}
-
-void MaxSat::printSolution(int* solution) {
-    cout << "[";
-    for(int i = 0; i < numVariables - 1; i++) {
-        cout << solution[i] << ", ";
-    }
-    cout << solution[numVariables - 1] << "]" << endl;
-}
-
-void MaxSat::printPopulation() {
-    cout << "Printing population (" << individuals << " individuals, " << numVariables << " size solution)..." << endl;
-    for(int i = 0; i < individuals; i++) {
-        printSolution(population[i]);
-    }
-}
-
-void MaxSat::printPV() {
-    cout << "Printing current population vector..." << endl;
-    cout << "[";
-    for(int i = 0; i < PV.size() - 1; i++) {
-        cout << PV[i] << ", ";
-    }
-    cout << PV[PV.size() - 1] << "]" << endl;
-}
-
-void MaxSat::printClauses() {
-    cout << "Printing clauses:" << endl;
-    for(int i = 0; i < clauses.size(); i++) {
-        for(int j = 0; j < clauses[0].size(); j++) {
-            // is it possible for clauses[0].size() to cause error in extreme cases?
-            cout << clauses[i].at(j) << ", ";
-        }
-        cout << "\n";
-    }
-}
-
-void MaxSat::solvePBIL() {
-	srand(time(NULL));
-	
-	// initialize PV
-	initPV();
-    
-    double randNum;
-    // keeps track of the most clauses satisfied over the whole process
-    bestValue = 0;
-	
-	int genRemaining = generations;
-	
-	while(genRemaining >= 0) {
-		// create population
-		for(int i = 0; i < individuals; i++) {
-			for(int j = 0; j < numVariables; j++) {
-				randNum = ((double) rand())/(INT_MAX);
-				if(randNum < PV[j]) {
-					population[i][j] = 1;
-				} else {
-					population[i][j] = 0;
-				}
-			}
-		}
-        // evaluate fitness of generated population
-		evalFitness();
-		int bestFitness = findMaxFitness();
-		int worstFitness = findMinFitness();
-		
-		// update PV towards best solution
-		for(int i = 0; i < PV.size(); i++) {
-			PV[i] = PV[i] * (1.0 - posRate) + population[bestFitness][i] * posRate;
-		}
-		// update PV away from worst solution
-		for(int i = 0; i < PV.size(); i++) {
-			if(population[bestFitness][i] != population[worstFitness][i]) {
-				// only change the values that differ from the best solution's values
-				PV[i] = PV[i] * (1.0 - negRate) + population[bestFitness][i] * negRate;
-			}
-		}
-		
-		// mutate!
-		mutatePV();
-        
-        if(fitnessList[bestFitness] > bestValue) {
-            bestValue = fitnessList[bestFitness];
-            genFound = generations - genRemaining;
-        }
-        
-		genRemaining--;
-	}
-}
-
-int compare ( const void *pa, const void *pb ) {
-	const int *a = *(const int **)pa;
-	const int *b = *(const int **)pb;
-	
-	return a[1] - b[1];
 }
 
 //array copy is used to deal with moving malloced arrays
@@ -650,6 +381,27 @@ void MaxSat::uniformCross() {
 	}
 }
 
+void MaxSat::mutatePV() {
+    double mutRand;
+    int dirRand;
+    int mutDirection;
+    for(int i = 0; i < PV.size() - 1; i++) {
+        mutRand = ((double) rand())/(RAND_MAX);
+        // mutate or not?
+        if(mutRand < pM) {
+            dirRand = rand() % 100;
+            // choose mutation direction
+            if((double) dirRand / 100 < 0.5) {
+                mutDirection = 1;
+            } else {
+                mutDirection = 0;
+            }
+            // make mutation
+            PV[i] = PV[i] * (1.0 - mutAmnt) + mutDirection * mutAmnt;
+        }
+    }
+}
+
 void MaxSat::mutateOffspring() {
 	int mutRand;
 	for(int i = 0; i < individuals; i++) {
@@ -663,15 +415,91 @@ void MaxSat::mutateOffspring() {
 	}
 }
 
-void MaxSat::initPopulation() {
-	//initialize population for GA with each individual
-	//holding a random value of 1 or 0
-	int randNum;
-	for (int i = 0; i < individuals; i++) {
-		for (int j = 0; j < numVariables; j++) {
-			randNum = rand() % 2;
-			population[i][j] = randNum;
+void MaxSat::printSolution(int* solution) {
+    cout << "[";
+    for(int i = 0; i < numVariables - 1; i++) {
+        cout << solution[i] << ", ";
+    }
+    cout << solution[numVariables - 1] << "]" << endl;
+}
+
+void MaxSat::printPopulation() {
+    cout << "Printing population (" << individuals << " individuals, " << numVariables << " size solution)..." << endl;
+    for(int i = 0; i < individuals; i++) {
+        printSolution(population[i]);
+    }
+}
+
+void MaxSat::printPV() {
+    cout << "Printing current population vector..." << endl;
+    cout << "[";
+    for(int i = 0; i < PV.size() - 1; i++) {
+        cout << PV[i] << ", ";
+    }
+    cout << PV[PV.size() - 1] << "]" << endl;
+}
+
+void MaxSat::printClauses() {
+    cout << "Printing clauses:" << endl;
+    for(int i = 0; i < clauses.size(); i++) {
+        for(int j = 0; j < clauses[0].size(); j++) {
+            // is it possible for clauses[0].size() to cause error in extreme cases?
+            cout << clauses[i].at(j) << ", ";
+        }
+        cout << "\n";
+    }
+}
+
+void MaxSat::solvePBIL() {
+	srand(time(NULL));
+	
+	// initialize PV
+	initPV();
+    
+    double randNum;
+    // keeps track of the most clauses satisfied over the whole process
+    bestValue = 0;
+	
+	int genRemaining = generations;
+	
+	while(genRemaining >= 0) {
+		// create population
+		for(int i = 0; i < individuals; i++) {
+			for(int j = 0; j < numVariables; j++) {
+				randNum = ((double) rand())/(INT_MAX);
+				if(randNum < PV[j]) {
+					population[i][j] = 1;
+				} else {
+					population[i][j] = 0;
+				}
+			}
 		}
+        // evaluate fitness of generated population
+		evalFitness();
+		int bestFitness = findMaxFitness();
+		int worstFitness = findMinFitness();
+		
+		// update PV towards best solution
+		for(int i = 0; i < PV.size(); i++) {
+			PV[i] = PV[i] * (1.0 - posRate) + population[bestFitness][i] * posRate;
+		}
+		// update PV away from worst solution
+		for(int i = 0; i < PV.size(); i++) {
+			if(population[bestFitness][i] != population[worstFitness][i]) {
+				// only change the values that differ from the best solution's values
+				PV[i] = PV[i] * (1.0 - negRate) + population[bestFitness][i] * negRate;
+			}
+		}
+		
+		// mutate!
+		mutatePV();
+        
+        if(fitnessList[bestFitness] > bestValue) {
+            bestValue = fitnessList[bestFitness];
+            genFound = generations - genRemaining;
+        }
+        
+		genRemaining--;
 	}
 }
 
